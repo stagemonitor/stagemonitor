0eb383eebe666df9fca828824f4938bdf70db396
felixbarnsteiner <f.barnsteiner@isys-software.de>
2014-01-15 18:32:11 +0100

    parsing JSON with Jackson instead of JsonSlurper

--- jawap-server/src/main/groovy/de/isys/jawap/server/util/ExtensionMethods.groovy
+++ jawap-server/src/main/groovy/de/isys/jawap/server/util/ExtensionMethods.groovy
@@ -1,13 +1,14 @@
 package de.isys.jawap.server.util
 
 import groovy.json.JsonSlurper
+import org.codehaus.jackson.map.ObjectMapper
 import org.springframework.core.io.ClassPathResource
 
 
 class ExtensionMethods {
 
 	public static def getJson(String self) {
-		new JsonSlurper().parseText(self)
+		new ObjectMapper().readValue(self, Object.class)
 	}
 
 	public static def loadJsonResource(String location) {
