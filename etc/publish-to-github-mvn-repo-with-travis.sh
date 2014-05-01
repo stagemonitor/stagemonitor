c0c3f9b0bd03ed921bb6359d10017183d5821af8
Felix <felix.barnsteiner@gmail.com>
2014-05-01 23:00:32 +0200

    fixed mvnRepo location

--- etc/publish-to-github-mvn-repo-with-travis.sh
+++ etc/publish-to-github-mvn-repo-with-travis.sh
@@ -7,15 +7,14 @@ echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
 
 # no SNAPSHOT versions, only relase tags/branches, no forks, no pull requests
 if ! grep -Fq SNAPSHOT gradle.properties && [[ "$TRAVIS_BRANCH" =~ [0-9]+\.[0-9]+\.[0-9]+ ]] && [ "$TRAVIS_REPO_SLUG" == "stagemonitor/stagemonitor" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  ./gradlew publish -PmvnRepo=build/repo
-  cp -R build/repo $HOME/repo
+  ./gradlew publish -PmvnRepo=$HOME/repo
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  # pssh! don't tell the secret!
+  # pssh, don't tell the secret!
   git clone --quiet https://${GH_TOKEN}@github.com/stagemonitor/mvn-repo > /dev/null
   cd mvn-repo
-  cp -R $HOME/repo .
+  cp -R $HOME/repo/* .
   git add .
   git commit -m "release $TRAVIS_REPO_SLUG $TRAVIS_BRANCH"
   git push -fq origin master > /dev/null  
