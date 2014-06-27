4d25adb4fa3a77ed9d7ff0cdf724b94ae9c324e1
felixbarnsteiner <f.barnsteiner@isys-software.de>
2014-06-27 13:24:09 +0200

    fixed test

--- stagemonitor-requestmonitor/src/test/java/org/stagemonitor/requestmonitor/profiler/CallStackElementTest.java
+++ stagemonitor-requestmonitor/src/test/java/org/stagemonitor/requestmonitor/profiler/CallStackElementTest.java
@@ -7,7 +7,7 @@ public class CallStackElementTest {
 
 	@Test
 	public void printPercentAsBar() throws Exception {
-		Assert.assertEquals("|||||     ", CallStackElement.printPercentAsBar(0.54d, 10, false));
+		Assert.assertEquals("|||||-----", CallStackElement.printPercentAsBar(0.54d, 10, false));
 		// █████▓▒▒▒▒
 		char[] chars = {9608, 9608, 9608, 9608, 9608, 9619, 9617, 9617, 9617, 9617};
 		Assert.assertEquals(new String(chars), CallStackElement.printPercentAsBar(0.56d, 10, true));
