9c0c03894853e7dce9760fd69b9dd25d0cddc36d
Felix <felix.barnsteiner@gmail.com>
2014-07-12 14:56:05 +0200

    refactored ConnectionMonitor and ConnectionMonitorAspect

--- stagemonitor-jdbc/src/main/java/org/stagemonitor/jdbc/ConnectionMonitor.java
+++ stagemonitor-jdbc/src/main/java/org/stagemonitor/jdbc/ConnectionMonitor.java
@@ -1,10 +1,15 @@
 package org.stagemonitor.jdbc;
 
 import com.codahale.metrics.MetricRegistry;
+import com.p6spy.engine.spy.P6Core;
+import com.p6spy.engine.spy.P6SpyLoadableOptions;
+import com.p6spy.engine.spy.P6SpyOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.stagemonitor.core.StageMonitor;
 import org.stagemonitor.core.util.GraphiteSanitizer;
+import org.stagemonitor.jdbc.p6spy.P6SpyMultiLogger;
+import org.stagemonitor.jdbc.p6spy.StagemonitorP6Logger;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -18,29 +23,63 @@ import static com.codahale.metrics.MetricRegistry.name;
 
 public class ConnectionMonitor {
 
-	private final static Logger log = LoggerFactory.getLogger(ConnectionMonitor.class);
+	private static final boolean ACTIVE = ConnectionMonitor.isActive();
 
-	private static ConcurrentMap<DataSource, String> dataSourceUrlMap = new ConcurrentHashMap<DataSource, String>();
-	private static MetricRegistry metricRegistry = StageMonitor.getMetricRegistry();
+	private final Logger logger = LoggerFactory.getLogger(ConnectionMonitor.class);
 
-	public static void monitorGetConnection(Connection connection, DataSource dataSource, long duration) {
-		if (connection != null) {
-			ensureUrlExistsForDataSource(dataSource, connection);
-			String url = dataSourceUrlMap.get(dataSource);
-			metricRegistry.timer(name("getConnection", url)).update(duration, TimeUnit.NANOSECONDS);
+	private ConcurrentMap<DataSource, String> dataSourceUrlMap = new ConcurrentHashMap<DataSource, String>();
+
+	private MetricRegistry metricRegistry;
+
+	private final boolean p6SpyAlreadyConfigured;
+
+	public ConnectionMonitor(MetricRegistry metricRegistry) {
+		this.metricRegistry = metricRegistry;
+
+		if (ACTIVE && StageMonitor.getConfiguration().collectSql()) {
+			P6SpyLoadableOptions options = P6SpyOptions.getActiveInstance();
+			addStagemonitorLogger(options);
+			p6SpyAlreadyConfigured = options.getDriverNames() == null || options.getDriverNames().isEmpty();
+			if (!p6SpyAlreadyConfigured) {
+				logger.info("Stagemonitor will not wrap connections with p6spy wrappers, because p6spy is already " +
+						"configured in your application");
+			}
+			P6Core.initialize();
+		} else {
+			p6SpyAlreadyConfigured = false;
 		}
 	}
 
-	private static DataSource ensureUrlExistsForDataSource(DataSource dataSource, Connection connection) {
+	private void addStagemonitorLogger(P6SpyLoadableOptions options) {
+		P6SpyMultiLogger.addLogger(new StagemonitorP6Logger());
+		if (p6SpyAlreadyConfigured) {
+			P6SpyMultiLogger.addLogger(options.getAppenderInstance());
+		}
+		options.setAppender(P6SpyMultiLogger.class.getCanonicalName());
+	}
+
+	public Connection monitorGetConnection(Connection connection, DataSource dataSource, long duration) throws SQLException {
+		ensureUrlExistsForDataSource(dataSource, connection);
+		String url = dataSourceUrlMap.get(dataSource);
+		metricRegistry.timer(name("getConnection", url)).update(duration, TimeUnit.NANOSECONDS);
+		return p6SpyAlreadyConfigured ? P6Core.wrapConnection(connection) : connection;
+	}
+
+	private DataSource ensureUrlExistsForDataSource(DataSource dataSource, Connection connection) {
 		if (!dataSourceUrlMap.containsKey(dataSource)) {
 			final DatabaseMetaData metaData;
 			try {
 				metaData = connection.getMetaData();
 				dataSourceUrlMap.put(dataSource, GraphiteSanitizer.sanitizeGraphiteMetricSegment(metaData.getURL() + "-" + metaData.getUserName()));
 			} catch (SQLException e) {
-				log.warn(e.getMessage(), e);
+				logger.warn(e.getMessage(), e);
 			}
 		}
 		return dataSource;
+	}
+
+	public static boolean isActive() {
+		return !StageMonitor.getConfiguration().getDisabledPlugins().contains(JdbcPlugin.class.getSimpleName()) &&
+				StageMonitor.getConfiguration().isStagemonitorActive();
 	}
 }
--- stagemonitor-jdbc/src/main/java/org/stagemonitor/jdbc/ConnectionMonitorAspect.java
+++ stagemonitor-jdbc/src/main/java/org/stagemonitor/jdbc/ConnectionMonitorAspect.java
@@ -1,17 +1,10 @@
 package org.stagemonitor.jdbc;
 
-import com.p6spy.engine.spy.P6Core;
-import com.p6spy.engine.spy.P6SpyLoadableOptions;
-import com.p6spy.engine.spy.P6SpyOptions;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.stagemonitor.core.StageMonitor;
-import org.stagemonitor.jdbc.p6spy.P6SpyMultiLogger;
-import org.stagemonitor.jdbc.p6spy.StagemonitorP6Logger;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -19,34 +12,13 @@ import java.sql.Connection;
 @Aspect
 public class ConnectionMonitorAspect {
 
-	public static final boolean ACTIVE = !StageMonitor.getConfiguration().getDisabledPlugins()
-			.contains(JdbcPlugin.class.getSimpleName()) && StageMonitor.getConfiguration().isStagemonitorActive();
+	private static final boolean ACTIVE = ConnectionMonitor.isActive();
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private ConnectionMonitor connectionMonitor;
 
-	private final boolean isP6SpyAlreadyConfigured;
 
 	public ConnectionMonitorAspect() {
-		if (ACTIVE && StageMonitor.getConfiguration().collectSql()) {
-			P6SpyLoadableOptions options = P6SpyOptions.getActiveInstance();
-			addStagemonitorLogger(options);
-			isP6SpyAlreadyConfigured = options.getDriverNames() == null || options.getDriverNames().isEmpty();
-			if (!isP6SpyAlreadyConfigured) {
-				logger.info("Stagemonitor will not wrap connections with p6spy wrappers, because p6spy is already " +
-						"configured in your application");
-			}
-			P6Core.initialize();
-		} else {
-			isP6SpyAlreadyConfigured = false;
-		}
-	}
-
-	private void addStagemonitorLogger(P6SpyLoadableOptions options) {
-		P6SpyMultiLogger.addLogger(new StagemonitorP6Logger());
-		if (isP6SpyAlreadyConfigured) {
-			P6SpyMultiLogger.addLogger(options.getAppenderInstance());
-		}
-		options.setAppender(P6SpyMultiLogger.class.getCanonicalName());
+		connectionMonitor = new ConnectionMonitor(StageMonitor.getMetricRegistry());
 	}
 
 	@Pointcut(value = "call(java.sql.Connection javax.sql.DataSource.getConnection(..)) && !within(javax.sql.DataSource+)")
@@ -60,17 +32,8 @@ public class ConnectionMonitorAspect {
 
 	@Around(value = "ifActivated() && dataSourceConnection()")
 	public Object aroundGetConnection(ProceedingJoinPoint pjp) throws Throwable {
-		Connection connection = null;
 		final long start = System.nanoTime();
-		try {
-			connection = (Connection) pjp.proceed();
-			return isP6SpyAlreadyConfigured ? P6Core.wrapConnection(connection) : connection;
-		} finally {
-			long duration = System.nanoTime() - start;
-			if (connection != null) {
-				DataSource dataSource = (DataSource) pjp.getTarget();
-				ConnectionMonitor.monitorGetConnection(connection, dataSource, duration);
-			}
-		}
+		Connection connection = (Connection) pjp.proceed();
+		return connectionMonitor.monitorGetConnection(connection, (DataSource) pjp.getTarget(), System.nanoTime() - start);
 	}
 }
