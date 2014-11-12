fcf8490367ce6f79dda9b4ae40b96dccb00ed42e
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2014-11-12 13:50:53 +0100

    added ConditionalTravisTestRunner to MeasurementSessionTest

--- stagemonitor-core/src/test/java/org/stagemonitor/core/MeasurementSessionTest.java
+++ stagemonitor-core/src/test/java/org/stagemonitor/core/MeasurementSessionTest.java
@@ -1,10 +1,13 @@
 package org.stagemonitor.core;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.stagemonitor.junit.ConditionalTravisTestRunner;
 import org.stagemonitor.junit.ExcludeOnTravis;
 
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(ConditionalTravisTestRunner.class)
 public class MeasurementSessionTest {
 
 	@Test
