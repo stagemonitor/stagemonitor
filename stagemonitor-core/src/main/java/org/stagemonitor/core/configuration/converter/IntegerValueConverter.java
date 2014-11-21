aac9026626efda5b57e6ccbb70b68a59a30faf90
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2014-11-21 09:29:47 +0100

    fixed error message

--- stagemonitor-core/src/main/java/org/stagemonitor/core/configuration/converter/IntegerValueConverter.java
+++ stagemonitor-core/src/main/java/org/stagemonitor/core/configuration/converter/IntegerValueConverter.java
@@ -7,7 +7,7 @@ public class IntegerValueConverter implements ValueConverter<Integer> {
 		try {
 			return Integer.valueOf(s);
 		} catch (RuntimeException e) {
-			throw new IllegalArgumentException("Can't '" + s + "' to Long.", e);
+			throw new IllegalArgumentException("Can't convert '" + s + "' to Integer.", e);
 		}
 	}
 
