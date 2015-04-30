3c52565ab433188db647d820f661b2638a317e9f
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2015-04-30 12:02:14 +0200

    Only use jackson afterburner if it has the same version as databind

--- stagemonitor-core/src/main/java/org/stagemonitor/core/util/JsonUtils.java
+++ stagemonitor-core/src/main/java/org/stagemonitor/core/util/JsonUtils.java
@@ -6,9 +6,11 @@ import java.util.concurrent.TimeUnit;
 
 import com.codahale.metrics.json.MetricsModule;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,15 +21,26 @@ public class JsonUtils {
 	private static final ObjectMapper MAPPER = new ObjectMapper();
 
 	static {
-		// Avoiding java.lang.NoSuchMethodError: com.fasterxml.jackson.databind.ser.BeanPropertyWriter.isUnwrapping()
-		// This happens, if the version of jackson databind is < 2.3.0.
-		// Because maven resolves a version conflict with the nearest-wins strategy it is possible that
-		// jackson-module-afterburner is in a higher version that jackson-databind and jackson-core
-//		if (MAPPER.version().compareTo(new Version(2, 3, 0, null, "com.fasterxml.jackson.core", "jackson-databind")) >= 0) {
-//			MAPPER.registerModule(new AfterburnerModule());
-//		}
+		/*
+		 * Avoiding java.lang.NoSuchMethodError: com.fasterxml.jackson.databind.ser.BeanPropertyWriter.isUnwrapping()
+		 * This happens, if the version of jackson databind less that the Afterburner version.
+		 *
+		 * One reason can be maven, because it resolves a version conflict with the nearest-wins strategy it is possible that
+		 * jackson-module-afterburner is in a higher version that jackson-databind and jackson-core
+		 *
+		 * Another reason could be that a application server bundled version of jackson databind is used
+		 */
+		if (versionsMatch(MAPPER.version(), new AfterburnerModule().version())) {
+			MAPPER.registerModule(new AfterburnerModule());
+		}
 		MAPPER.registerModule(new MetricsModule(TimeUnit.SECONDS, TimeUnit.MILLISECONDS, false));
 		MAPPER.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+	}
+
+	public static boolean versionsMatch(Version v1, Version v2) {
+		return v1.getMajorVersion() == v2.getMajorVersion() &&
+				v1.getMinorVersion() == v2.getMinorVersion() &&
+				v1.getPatchLevel() == v2.getPatchLevel();
 	}
 
 	public static String toJson(Object o) {
