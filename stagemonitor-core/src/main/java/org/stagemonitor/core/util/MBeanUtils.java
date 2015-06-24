34050b2d53831b67c8efc3cd314dfe25ba5300b3
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2015-06-24 22:29:44 +0200

    Implement MBeanUtils to easily monitor MBeans

---
+++ stagemonitor-core/src/main/java/org/stagemonitor/core/util/MBeanUtils.java
@@ -0,0 +1,99 @@
+package org.stagemonitor.core.util;
+
+import java.lang.management.ManagementFactory;
+import java.util.Set;
+
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import javax.management.QueryExp;
+
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.stagemonitor.core.Stagemonitor;
+
+public class MBeanUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(MBeanUtils.class);
+
+	private static final MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+
+	private static final MetricRegistry metricRegistry = Stagemonitor.getMetricRegistry();
+
+	private MBeanUtils() {
+	}
+
+	/**
+	 * @param objectName The object name pattern identifying the MBeans to be retrieved.
+	 * @return The ObjectInstance object for the selected MBean.
+	 * @throws java.util.NoSuchElementException
+	 *                              There are no matching MBeans
+	 * @throws NullPointerException The objectName parameter is null.
+	 * @throws RuntimeException     The string passed as a parameter does not have the right format
+	 * @see MBeanServer#queryMBeans(javax.management.ObjectName, javax.management.QueryExp)
+	 */
+	public static ObjectInstance queryMBean(String objectName) {
+		return queryMBeans(objectName).iterator().next();
+	}
+
+	/**
+	 * @param objectName The object name pattern identifying the MBeans to be retrieved.
+	 * @return The ObjectInstance object for the selected MBean.
+	 * @throws java.util.NoSuchElementException
+	 *                              There are no matching MBeans
+	 * @throws NullPointerException The objectName parameter is null.
+	 * @throws RuntimeException     The string passed as a parameter does not have the right format
+	 * @see MBeanServer#queryMBeans(javax.management.ObjectName, javax.management.QueryExp)
+	 */
+	public static Set<ObjectInstance> queryMBeans(String objectName) {
+		try {
+			return queryMBeans(new ObjectName(objectName), null);
+		} catch (MalformedObjectNameException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Gets MBeans controlled by the MBean server. This method allows any of the following to be obtained:
+	 * All MBeans, a set of MBeans specified by pattern matching on the ObjectName and/or a Query expression,
+	 * a specific MBean. When the object name is null or no domain and key properties are specified,
+	 * all objects are to be selected (and filtered if a query is specified).
+	 * It returns the set of ObjectInstance objects (containing the ObjectName and the Java Class name)
+	 * for the selected MBeans.
+	 *
+	 * @param name  The object name pattern identifying the MBeans to be retrieved.
+	 *              If null or no domain and key properties are specified, all the MBeans registered will
+	 *              be retrieved.
+	 * @param query The query expression to be applied for selecting MBeans.
+	 *              If null no query expression will be applied for selecting MBean
+	 * @return A set containing the ObjectInstance objects for the selected MBeans.
+	 *         If no MBean satisfies the query an empty list is returned.
+	 */
+	public static Set<ObjectInstance> queryMBeans(ObjectName objectName, QueryExp queryExp) {
+		return mbeanServer.queryMBeans(objectName, queryExp);
+	}
+
+	/**
+	 * Registers a MBean into the MetricRegistry
+	 *
+	 * @param objectInstance     The ObjectInstance
+	 * @param mBeanAttributeName The attribute name of the MBean that should be collected
+	 * @param metricName         The name of the metric in the MetricRegistry
+	 */
+	public static void registerMBean(final ObjectInstance objectInstance, final String mBeanAttributeName, String metricName) {
+		metricRegistry.register(metricName, new Gauge<Object>() {
+			@Override
+			public Object getValue() {
+				try {
+					return mbeanServer.getAttribute(objectInstance.getObjectName(), mBeanAttributeName);
+				} catch (Exception e) {
+					logger.warn(e.getMessage(), e);
+					return null;
+				}
+			}
+		});
+	}
+}
---
+++ stagemonitor-core/src/test/java/org/stagemonitor/core/util/MBeanUtilsTest.java
@@ -0,0 +1,28 @@
+package org.stagemonitor.core.util;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+import org.junit.*;
+import org.stagemonitor.core.Stagemonitor;
+
+public class MBeanUtilsTest {
+
+	private MetricRegistry metricRegistry = Stagemonitor.getMetricRegistry();
+
+	@BeforeClass
+	@AfterClass
+	public static void resetStagemonitor() {
+		Stagemonitor.reset();
+	}
+
+	@Test
+	public void testRegisterMBean() throws Exception {
+		MBeanUtils.registerMBean(MBeanUtils.queryMBean("java.lang:type=ClassLoading"), "LoadedClassCount", "jvm.classloading.count");
+		final Gauge classLoadingGauge = metricRegistry.getGauges().get("jvm.classloading.count");
+		assertNotNull(classLoadingGauge);
+		assertTrue(((Integer) classLoadingGauge.getValue()) > 1);
+	}
+}
