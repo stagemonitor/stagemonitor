bedc28cda0f0f3f406257f3d677ad0bfb9f58953
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2015-06-30 21:43:38 +0200

    Update javassist

--- stagemonitor-requestmonitor/src/main/java/org/stagemonitor/requestmonitor/profiler/ProfilingInstrumenter.java
+++ stagemonitor-requestmonitor/src/main/java/org/stagemonitor/requestmonitor/profiler/ProfilingInstrumenter.java
@@ -5,13 +5,9 @@ import java.lang.reflect.Modifier;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.NotFoundException;
-import javassist.bytecode.BadBytecode;
-import javassist.bytecode.CodeAttribute;
-import javassist.bytecode.CodeIterator;
-import javassist.bytecode.Opcode;
-import org.stagemonitor.core.util.ClassUtils;
 import org.stagemonitor.core.Stagemonitor;
 import org.stagemonitor.core.instrument.StagemonitorJavassistInstrumenter;
+import org.stagemonitor.core.util.ClassUtils;
 import org.stagemonitor.requestmonitor.RequestMonitorPlugin;
 
 public class ProfilingInstrumenter extends StagemonitorJavassistInstrumenter {
@@ -45,32 +41,10 @@ public class ProfilingInstrumenter extends StagemonitorJavassistInstrumenter {
 					&& ctClass.equals(m.getDeclaringClass())
 					&& !m.getName().contains("access$")) {
 
-				if (hasSwitchCase(m)) {
-					continue;
-				}
 				m.insertBefore("org.stagemonitor.requestmonitor.profiler.Profiler.start(\"" + getSignature(ctClass, m) + "\");");
 				m.insertAfter("org.stagemonitor.requestmonitor.profiler.Profiler.stop();", true);
 			}
 		}
-	}
-
-	/**
-	 * Javassist has problems when a method contains a return statement before a switch case.
-	 * Workaround for https://issues.jboss.org/browse/JASSIST-241
-	 *
-	 * @param m the method to check
-	 * @return <code>true</code>, if the method contains a switch case statement, <code>false</code> otherwise
-	 */
-	private boolean hasSwitchCase(CtMethod m) throws BadBytecode {
-		final CodeAttribute codeAttribute = m.getMethodInfo().getCodeAttribute();
-		for (CodeIterator ci = codeAttribute.iterator(); ci.hasNext(); ) {
-			int index = ci.next();
-			int op = ci.byteAt(index);
-			if (op == Opcode.TABLESWITCH || op == Opcode.LOOKUPSWITCH) {
-				return true;
-			}
-		}
-		return false;
 	}
 
 	private String getSignature(CtClass clazz, CtMethod method) throws NotFoundException {
