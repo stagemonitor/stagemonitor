71af54a42e5fba6c9cd445a2ebbefe5a8355f454
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2015-07-15 16:51:53 +0200

    Add release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Any subsequent commands which fail will cause the shell script to exit immediately
+set -e
+
+./gradlew release
+
+git push
+
+# check out the tag that was just created
+git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+
+./gradlew uploadArchives
+
