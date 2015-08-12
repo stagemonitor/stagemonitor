85eef60785dd2f9647b46611e3b66e882a480a77
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2015-08-12 11:49:46 +0200

    Partially migrate jvm plugin to Metric2Registry

--- stagemonitor-jvm/src/main/java/org/stagemonitor/jvm/JvmPlugin.java
+++ stagemonitor-jvm/src/main/java/org/stagemonitor/jvm/JvmPlugin.java
@@ -1,33 +1,34 @@
 package org.stagemonitor.jvm;
 
 
-import java.util.Arrays;
+import static org.stagemonitor.core.metrics.metrics2.MetricName.name;
+
+import java.util.Collections;
 import java.util.List;
 
 import com.codahale.metrics.Gauge;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
-import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.stagemonitor.core.CorePlugin;
 import org.stagemonitor.core.StagemonitorPlugin;
 import org.stagemonitor.core.configuration.Configuration;
 import org.stagemonitor.core.elasticsearch.ElasticsearchClient;
+import org.stagemonitor.core.metrics.metrics2.Metric2Registry;
 
 public class JvmPlugin extends StagemonitorPlugin {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Override
-	public void initializePlugin(MetricRegistry registry, Configuration configuration) {
-		registry.register("jvm.gc", new GarbageCollectorMetricSet());
-		registry.register("jvm.memory", new MemoryUsageGaugeSet());
+	public void initializePlugin(Metric2Registry registry, Configuration configuration) {
+		// TODO
+//		registry.register(name("jvm_gc").build(), new GarbageCollectorMetricSet());
+//		registry.register(name("jvm_memory").build(), new MemoryUsageGaugeSet());
 
 		final CpuUtilisationWatch cpuWatch;
 		try {
 			cpuWatch = new CpuUtilisationWatch();
 			cpuWatch.start();
-			registry.register("jvm.cpu.process.usage", new Gauge<Float>() {
+			registry.register(name("jvm_process_cpu_usage").build(), new Gauge<Float>() {
 				@Override
 				public Float getValue() {
 					try {
@@ -40,12 +41,6 @@ public class JvmPlugin extends StagemonitorPlugin {
 		} catch (Exception e) {
 			logger.warn("Could not register cpu usage. ({})", e.getMessage());
 		}
-		registry.register("online", new Gauge<Integer>() {
-			@Override
-			public Integer getValue() {
-				return 1;
-			}
-		});
 
 		ElasticsearchClient elasticsearchClient = configuration.getConfig(CorePlugin.class).getElasticsearchClient();
 		elasticsearchClient.sendGrafanaDashboardAsync("JVM Memory.json");
@@ -54,6 +49,6 @@ public class JvmPlugin extends StagemonitorPlugin {
 
 	@Override
 	public List<String> getPathsOfWidgetMetricTabPlugins() {
-		return Arrays.asList("/stagemonitor/static/tabs/metrics/jvm-metrics");
+		return Collections.singletonList("/stagemonitor/static/tabs/metrics/jvm-metrics");
 	}
 }
