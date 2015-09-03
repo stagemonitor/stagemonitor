cf019df030fefcb241c0c44503043fffcc163cf1
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2015-09-03 10:31:54 +0200

    Repair in browser widget request dashboard

--- stagemonitor-requestmonitor/src/main/resources/stagemonitor/static/tabs/metrics/request-metrics.js
+++ stagemonitor-requestmonitor/src/main/resources/stagemonitor/static/tabs/metrics/request-metrics.js
@@ -9,49 +9,49 @@
 				columns: [
 					{
 						metricCategory: "timers",
-						metricPathRegex: /response_time.([^\.]+).server.total/,
+						metricPathRegex: /response_time_server.([^\.]+).total/,
 						metric: "m1_rate",
 						title: "Requests/s"
 					},
 					{
 						metricCategory: "timers",
-						metricPathRegex: /response_time.([^\.]+).server.total/,
+						metricPathRegex: /response_time_server.([^\.]+).total/,
 						metric: "max",
 						title: "Max"
 					},
 					{
 						metricCategory: "timers",
-						metricPathRegex: /response_time.([^\.]+).server.total/,
+						metricPathRegex: /response_time_server.([^\.]+).total/,
 						metric: "mean",
 						title: "Mean"
 					},
 					{
 						metricCategory: "timers",
-						metricPathRegex: /response_time.([^\.]+).server.total/,
+						metricPathRegex: /response_time_server.([^\.]+).total/,
 						metric: "min",
 						title: "Min"
 					},
 					{
 						metricCategory: "timers",
-						metricPathRegex: /response_time.([^\.]+).server.total/,
+						metricPathRegex: /response_time_server.([^\.]+).total/,
 						metric: "p50",
 						title: "p50"
 					},
 					{
 						metricCategory: "timers",
-						metricPathRegex: /response_time.([^\.]+).server.total/,
+						metricPathRegex: /response_time_server.([^\.]+).total/,
 						metric: "p95",
 						title: "p95"
 					},
 					{
 						metricCategory: "timers",
-						metricPathRegex: /response_time.([^\.]+).server.total/,
+						metricPathRegex: /response_time_server.([^\.]+).total/,
 						metric: "stddev",
 						title: "Std. Dev."
 					},
 					{
 						metricCategory: "meters",
-						metricPathRegex: /response_time.([^\.]+).server.jdbc/,
+						metricPathRegex: /response_time_server.([^\.]+).jdbc/,
 						metric: "m1_rate",
 						title: "SQLs/sec"
 					}
@@ -66,7 +66,7 @@
 								format: 'ms',
 								fill: 0.1,
 								columns: [
-									{ metricCategory: "timers", metricPathRegex: "response_time.(${rowName}).server.total", metric: "mean" }
+									{ metricCategory: "timers", metricPathRegex: "response_time_server.(${rowName}).total", metric: "mean" }
 								]
 							}
 						},
@@ -77,7 +77,7 @@
 								format: 'requests/sec',
 								fill: 0.1,
 								columns: [
-									{ metricCategory: "timers", metricPathRegex: "response_time.(${rowName}).server.total", metric: "m1_rate"}
+									{ metricCategory: "timers", metricPathRegex: "response_time_server.(${rowName}).total", metric: "m1_rate"}
 								]
 							}
 						}
