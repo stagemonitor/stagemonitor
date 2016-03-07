ae2d8988df87bea76a6d97f6c8e6d75a02907784
Felix Barnsteiner <f.barnsteiner@isys-software.de>
2016-03-07 19:25:45 +0100

    Remove @Repeatable annotation
    So that stagemonitor can be compiled with java 6 and 7
    and that the integration tests work again

--- stagemonitor-alerting/src/main/java/org/stagemonitor/alerting/annotation/SLA.java
+++ stagemonitor-alerting/src/main/java/org/stagemonitor/alerting/annotation/SLA.java
@@ -1,7 +1,6 @@
 package org.stagemonitor.alerting.annotation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -17,7 +16,6 @@ import org.stagemonitor.alerting.check.Threshold;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@Repeatable(SLAs.class)
 public @interface SLA {
 
 	/**
