a16816bf813dc90c9bc7bdb2f840d1b84d55e839
Fabian Trampusch <f.trampusch@isys-software.de>
2016-05-04 16:28:47 +0200

    add validation

--- stagemonitor-grafana-elasticsearch/src/components/config.js
+++ stagemonitor-grafana-elasticsearch/src/components/config.js
@@ -7,6 +7,15 @@ export class ExampleAppConfigCtrl {
         this.isDatasourceCreated = false;
         this.appEditCtrl.setPreUpdateHook(this.preUpdate.bind(this));
 
+        this.appModel.jsonData = this.appModel.jsonData || {};
+        this.appModel.jsonData.elasticsearchUrl = this.appModel.jsonData.elasticsearchUrl || "";
+        this.appModel.jsonData.reportingInterval = this.appModel.jsonData.reportingInterval || ">60s";
+        this.validation = {
+            reportingIntervalValid: true,
+            elasticsearchUrlValid: true
+        };
+
+
         backendSrv.get('/public/plugins/stagemonitor-grafana-elasticsearch/plugin.json').then(data => {
             this.dashboards = _.filter(data.includes, {type: 'dashboard'});
         });
@@ -22,20 +31,36 @@ export class ExampleAppConfigCtrl {
 
     init() {
         this.backendSrv.get('/api/datasources')
-                       .then(datasources => {
-                           this.isDatasourceCreated = _.findIndex(datasources, {name: this.datasourceName}) !== -1
-                       });
+            .then(datasources => {
+                this.isDatasourceCreated = _.findIndex(datasources, {name: this.datasourceName}) !== -1
+            });
     }
 
     createDatasource() {
-        return this.backendSrv.post('/api/datasources', {
-            "name": this.datasourceName,
-            "type": "elasticsearch",
-            "url": this.appModel.jsonData.elasticsearchUrl,
-            "access": "proxy",
-            "jsonData": {"timeField": "@timestamp", "esVersion": 2, "interval": "Daily"},
-            "database": "[stagemonitor-metrics-]YYYY.MM.DD"
-        });
+
+        this.validation.elasticsearchUrlValid = !this.isEmptyOrUndefined(this.appModel.jsonData.elasticsearchUrl);
+        this.validation.reportingIntervalValid = !this.isEmptyOrUndefined(this.appModel.jsonData.reportingInterval);
+
+        if (_.values(this.validation).some(value => !value))  {
+            return Promise.reject();
+        } else {
+            return this.backendSrv.post('/api/datasources', {
+                "name": this.datasourceName,
+                "type": "elasticsearch",
+                "url": this.appModel.jsonData.elasticsearchUrl,
+                "access": "proxy",
+                "jsonData": {
+                    "timeField": "@timestamp",
+                    "esVersion": 2, "interval": "Daily",
+                    "timeInterval": this.appModel.jsonData.reportingInterval
+                },
+                "database": "[stagemonitor-metrics-]YYYY.MM.DD"
+            });
+        }
+    }
+
+    isEmptyOrUndefined(value) {
+        return value === undefined || value == null || value.trim() === "";
     }
 }
 
