3b5ca1522ed9c695e2a8645a20ce6c7f7d6aae63
Felix Barnsteiner <felix.barnsteiner@isys-software.de>
2016-09-04 12:58:11 +0200

    Make sure that the Freemarker profiler is only applied on versions >= 2.3.23
    where the Environment#getCurrentTemplate() method was introduced.
    This prevents nasty IllegalAccessErrors and NoSuchMethodErrors.

--- stagemonitor-requestmonitor/src/main/java/org/stagemonitor/requestmonitor/freemarker/FreemarkerProfilingTransformer.java
+++ stagemonitor-requestmonitor/src/main/java/org/stagemonitor/requestmonitor/freemarker/FreemarkerProfilingTransformer.java
@@ -7,6 +7,7 @@ import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
+import net.bytebuddy.matcher.ElementMatchers;
 
 import org.stagemonitor.core.instrument.StagemonitorByteBuddyTransformer;
 import org.stagemonitor.requestmonitor.profiler.CallStackElement;
@@ -35,23 +36,27 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
  *         `-- SELECT * from THINGS
  * </code>
  * </pre>
+ *
+ * Note that this will only be active when working with Freemarker versions starting at 2.3.23.
  */
 public class FreemarkerProfilingTransformer extends StagemonitorByteBuddyTransformer {
 
 	/**
-	 * Application code can be called by freemarker via the {@link freemarker.core.Dot} or the
-	 * {@link freemarker.core.MethodCall} classes.
+	 * Application code can be called by freemarker via the {@link freemarker.core.Dot} or the {@link
+	 * freemarker.core.MethodCall} classes.
 	 * <p/>
 	 * For example, when the expression ${templateModel.foo} is evaluated, {@link freemarker.core.Dot#_eval(Environment)}
 	 * evaluates <code>foo</code> by calling <code>TemplateModel#getFoo()</code>.
 	 * <p/>
-	 * The expression ${templateModel.getFoo()} will be evaluated a bit differently as {@link freemarker.core.Dot#_eval(Environment)}
-	 * only returns a reference to the method <code>TemplateModel#getFoo()</code> instead of calling it directly.
-	 * {@link freemarker.core.MethodCall#_eval(Environment)} then performs the actual call to <code>TemplateModel#getFoo()</code>.
+	 * The expression ${templateModel.getFoo()} will be evaluated a bit differently as {@link
+	 * freemarker.core.Dot#_eval(Environment)} only returns a reference to the method
+	 * <code>TemplateModel#getFoo()</code> instead of calling it directly. {@link freemarker.core.MethodCall#_eval(Environment)}
+	 * then performs the actual call to <code>TemplateModel#getFoo()</code>.
 	 */
 	@Override
 	protected ElementMatcher.Junction<TypeDescription> getIncludeTypeMatcher() {
-		return named("freemarker.core.Dot").or(named("freemarker.core.MethodCall"));
+		return named("freemarker.core.Dot")
+				.or(named("freemarker.core.MethodCall"));
 	}
 
 	@Override
@@ -88,5 +93,31 @@ public class FreemarkerProfilingTransformer extends StagemonitorByteBuddyTransfo
 		if (currentFreemarkerCall != null && currentFreemarkerCall.getChildren().isEmpty()) {
 			currentFreemarkerCall.remove();
 		}
+	}
+
+	/**
+	 * Makes sure that this transformer is only applied on Freemarker versions >= 2.3.23 where the
+	 * {@link Environment#getCurrentTemplate()} method was made public. This prevents nasty
+	 * {@link IllegalAccessError}s and {@link NoSuchMethodError}s.
+	 */
+	@Override
+	public boolean isActive() {
+		try {
+			return hasMethodThat(named("getCurrentTemplate")
+					.and(ElementMatchers.<MethodDescription.InDefinedShape>isPublic())
+					.and(takesArguments(0)))
+					.matches(new TypeDescription.ForLoadedType(Class.forName("freemarker.core.Environment")));
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+	}
+
+	private ElementMatcher.Junction<TypeDescription> hasMethodThat(final ElementMatcher<MethodDescription.InDefinedShape> methodElementMatcher) {
+		return new ElementMatcher.Junction.AbstractBase<TypeDescription>() {
+			@Override
+			public boolean matches(TypeDescription target) {
+				return !target.getDeclaredMethods().filter(methodElementMatcher).isEmpty();
+			}
+		};
 	}
 }
