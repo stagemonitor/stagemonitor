c597ab5cae6ed6ae55e14ba5f9900ae3477bb03a
Felix Barnsteiner <felix.barnsteiner@isys.de>
2017-02-23 17:22:11 +0100

    Verify that no one sets sampling.priority to 0 in test
    Travis y u do dis?

--- stagemonitor-requestmonitor/src/test/java/org/stagemonitor/requestmonitor/reporter/ElasticsearchSpanReporterIntegrationTest.java
+++ stagemonitor-requestmonitor/src/test/java/org/stagemonitor/requestmonitor/reporter/ElasticsearchSpanReporterIntegrationTest.java
@@ -15,15 +15,20 @@ import org.stagemonitor.core.util.JsonUtils;
 import org.stagemonitor.requestmonitor.MonitoredMethodRequest;
 import org.stagemonitor.requestmonitor.RequestMonitor;
 import org.stagemonitor.requestmonitor.RequestMonitorPlugin;
+import org.stagemonitor.requestmonitor.tracing.wrapper.SpanInterceptor;
+import org.stagemonitor.requestmonitor.tracing.wrapper.SpanWrappingTracer;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 import io.opentracing.Span;
+import io.opentracing.tag.Tags;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -44,7 +49,21 @@ public class ElasticsearchSpanReporterIntegrationTest extends AbstractElasticsea
 		when(requestMonitorPlugin.isPseudonymizeUserNames()).thenReturn(true);
 		reporter = new ElasticsearchSpanReporter();
 		reporter.init(new SpanReporter.InitArguments(configuration, mock(Metric2Registry.class)));
-		when(requestMonitorPlugin.getTracer()).thenReturn(new Tracer.Builder(getClass().getSimpleName(), new NoopReporter(), new ConstSampler(true)).build());
+		final Tracer jaegerTracer = new Tracer.Builder(getClass().getSimpleName(), new NoopReporter(), new ConstSampler(true)).build();
+		when(requestMonitorPlugin.getTracer()).thenReturn(new SpanWrappingTracer(jaegerTracer, Collections.singletonList(new Callable<SpanInterceptor>() {
+			@Override
+			public SpanInterceptor call() throws Exception {
+				return new SpanInterceptor() {
+					@Override
+					public Number onSetTag(String key, Number value) {
+						if (Tags.SAMPLING_PRIORITY.getKey().equals(key) && value.intValue() == 0) {
+							fail();
+						}
+						return value;
+					}
+				};
+			}
+		})));
 	}
 
 	@Test
@@ -54,7 +73,7 @@ public class ElasticsearchSpanReporterIntegrationTest extends AbstractElasticsea
 		parameters.put("attr", "bla");
 		parameters.put("foo", "bar");
 		final MonitoredMethodRequest monitoredMethodRequest = new MonitoredMethodRequest(configuration, "Test#test", null, parameters);
-		final Span span = monitoredMethodRequest.createSpan(mock(RequestMonitor.RequestInformation.class));
+		final Span span = monitoredMethodRequest.createSpan(null);
 		span.setTag("foo.bar", "baz");
 		span.finish();
 		reporter.report(RequestMonitor.RequestInformation.of(span, null, Collections.<String, Object>emptyMap()));
