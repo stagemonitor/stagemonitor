e72b86f5240696a74d0013dc07123b8b360f7f9a
Felix Barnsteiner <felix.barnsteiner@isys.de>
2017-09-07 13:30:44 +0200

    Don't crash on one word sql statements
    closes #314

---
+++ stagemonitor-jdbc/src/test/java/org/stagemonitor/jdbc/StagemonitorJdbcEventListenerTest.java
@@ -0,0 +1,18 @@
+package org.stagemonitor.jdbc;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+public class StagemonitorJdbcEventListenerTest {
+	
+	@Test
+	public void getMethod() throws Exception {
+		assertSoftly(softly -> {
+			softly.assertThat(StagemonitorJdbcEventListener.getMethod("select * from stagemonitor")).isEqualTo("SELECT");
+			softly.assertThat(StagemonitorJdbcEventListener.getMethod("update stagemonitor set bug=false")).isEqualTo("UPDATE");
+			softly.assertThat(StagemonitorJdbcEventListener.getMethod("Commit")).isEqualTo("COMMIT");
+		});
+	}
+
+}
