38191d509cef61f0507497b9f17ed4de745e4356
Felix Barnsteiner <felix.barnsteiner@isys.de>
2017-09-07 13:38:42 +0200

    Add null check to StagemonitorJdbcEventListener#getMethod and trim() sql
    see #314

--- stagemonitor-jdbc/src/test/java/org/stagemonitor/jdbc/StagemonitorJdbcEventListenerTest.java
+++ stagemonitor-jdbc/src/test/java/org/stagemonitor/jdbc/StagemonitorJdbcEventListenerTest.java
@@ -10,8 +10,11 @@ public class StagemonitorJdbcEventListenerTest {
 	public void getMethod() throws Exception {
 		assertSoftly(softly -> {
 			softly.assertThat(StagemonitorJdbcEventListener.getMethod("select * from stagemonitor")).isEqualTo("SELECT");
-			softly.assertThat(StagemonitorJdbcEventListener.getMethod("update stagemonitor set bug=false")).isEqualTo("UPDATE");
+			softly.assertThat(StagemonitorJdbcEventListener.getMethod(" update stagemonitor set bug=false")).isEqualTo("UPDATE");
 			softly.assertThat(StagemonitorJdbcEventListener.getMethod("Commit")).isEqualTo("COMMIT");
+			softly.assertThat(StagemonitorJdbcEventListener.getMethod("")).isEqualTo("");
+			softly.assertThat(StagemonitorJdbcEventListener.getMethod(" ")).isEqualTo("");
+			softly.assertThat(StagemonitorJdbcEventListener.getMethod(null)).isNull();
 		});
 	}
 
