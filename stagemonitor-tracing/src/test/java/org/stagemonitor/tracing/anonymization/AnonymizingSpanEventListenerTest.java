b151eb947806bd9517f44a29a336b89eb3f49768
Fabian Trampusch <fabian.trampusch@freenet.de>
2017-09-12 10:20:11 +0200

    Add test for anonymizing IPv6

--- stagemonitor-tracing/src/test/java/org/stagemonitor/tracing/anonymization/AnonymizingSpanEventListenerTest.java
+++ stagemonitor-tracing/src/test/java/org/stagemonitor/tracing/anonymization/AnonymizingSpanEventListenerTest.java
@@ -10,6 +10,7 @@ import org.stagemonitor.tracing.utils.SpanUtils;
 import org.stagemonitor.tracing.wrapper.SpanWrapper;
 
 import java.net.Inet4Address;
+import java.net.Inet6Address;
 import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,8 +23,10 @@ import static org.mockito.Mockito.when;
 
 public class AnonymizingSpanEventListenerTest {
 
-	private static final Inet4Address IP = (Inet4Address) InetAddresses.forString("123.123.123.123");
-	private static final Inet4Address IP_ANONYMIZED = (Inet4Address) InetAddresses.forString("123.123.123.0");
+	private static final Inet4Address IPV4 = (Inet4Address) InetAddresses.forString("123.123.123.123");
+	private static final Inet4Address IPV4_ANONYMIZED = (Inet4Address) InetAddresses.forString("123.123.123.0");
+	private static final Inet6Address IPV6 = (Inet6Address) InetAddresses.forString("2001:0db8:85a3:0000:0000:8a2e:0370:7334");
+	private static final Inet6Address IPV6_ANONYMIZED = (Inet6Address) InetAddresses.forString("2001:0db8:85a3:0000:0000:0000:0000:0000");
 	private TracingPlugin tracingPlugin;
 
 	private MockTracer mockTracer = new MockTracer();
@@ -42,27 +45,39 @@ public class AnonymizingSpanEventListenerTest {
 	}
 
 	@Test
-	public void testAnonymizeUserNameAndIp() throws Exception {
+	public void testAnonymizeUserNameAndIpV4() throws Exception {
 		when(tracingPlugin.isPseudonymizeUserNames()).thenReturn(true);
 		when(tracingPlugin.isAnonymizeIPs()).thenReturn(true);
 
-		final SpanWrapper span = createSpan();
+		final SpanWrapper span = createSpanIpv4();
 
 		assertThat(span.getTags()).containsEntry("username", "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3");
 		assertThat(span.getTags()).doesNotContainKey("username_disclosed");
-		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IP_ANONYMIZED));
+		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IPV4_ANONYMIZED));
 	}
 
 	@Test
-	public void testAnonymizeIp() throws Exception {
+	public void testAnonymizeIpv6() throws Exception {
 		when(tracingPlugin.isPseudonymizeUserNames()).thenReturn(false);
 		when(tracingPlugin.isAnonymizeIPs()).thenReturn(true);
 
-		final SpanWrapper span = createSpan();
+		final SpanWrapper span = createSpan("test", IPV6.getHostAddress());
 
 		assertThat(span.getTags()).containsEntry("username", "test");
 		assertThat(span.getTags()).doesNotContainKey("username_disclosed");
-		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IP_ANONYMIZED));
+		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV6.getKey(), IPV6_ANONYMIZED.getHostAddress());
+	}
+
+	@Test
+	public void testAnonymizeIpV4() throws Exception {
+		when(tracingPlugin.isPseudonymizeUserNames()).thenReturn(false);
+		when(tracingPlugin.isAnonymizeIPs()).thenReturn(true);
+
+		final SpanWrapper span = createSpanIpv4();
+
+		assertThat(span.getTags()).containsEntry("username", "test");
+		assertThat(span.getTags()).doesNotContainKey("username_disclosed");
+		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IPV4_ANONYMIZED));
 	}
 
 	@Test
@@ -71,11 +86,11 @@ public class AnonymizingSpanEventListenerTest {
 		when(tracingPlugin.isAnonymizeIPs()).thenReturn(true);
 		when(tracingPlugin.getDiscloseUsers()).thenReturn(Collections.singleton("a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"));
 
-		final SpanWrapper span = createSpan();
+		final SpanWrapper span = createSpanIpv4();
 
 		assertThat(span.getTags()).containsEntry("username", "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3");
 		assertThat(span.getTags()).containsEntry("username_disclosed", "test");
-		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IP));
+		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IPV4));
 	}
 
 	@Test
@@ -84,12 +99,12 @@ public class AnonymizingSpanEventListenerTest {
 		when(tracingPlugin.isAnonymizeIPs()).thenReturn(true);
 		when(tracingPlugin.getDiscloseUsers()).thenReturn(Collections.singleton("test"));
 
-		final SpanWrapper span = createSpan();
+		final SpanWrapper span = createSpanIpv4();
 
 
 		assertThat(span.getTags()).containsEntry("username", "test");
 		assertThat(span.getTags()).containsEntry("username_disclosed", "test");
-		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IP));
+		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IPV4));
 	}
 
 	@Test
@@ -109,11 +124,11 @@ public class AnonymizingSpanEventListenerTest {
 		when(tracingPlugin.isPseudonymizeUserNames()).thenReturn(true);
 		when(tracingPlugin.isAnonymizeIPs()).thenReturn(true);
 
-		final SpanWrapper span = createSpan(null, IP.getHostAddress());
+		final SpanWrapper span = createSpan(null, IPV4.getHostAddress());
 
 		assertThat(span.getTags()).doesNotContainKey("username");
 		assertThat(span.getTags()).doesNotContainKey("username_disclosed");
-		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IP_ANONYMIZED));
+		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IPV4_ANONYMIZED));
 	}
 
 	@Test
@@ -133,19 +148,22 @@ public class AnonymizingSpanEventListenerTest {
 		when(tracingPlugin.isPseudonymizeUserNames()).thenReturn(false);
 		when(tracingPlugin.isAnonymizeIPs()).thenReturn(false);
 
-		final SpanWrapper span = createSpan();
+		final SpanWrapper span = createSpanIpv4();
 
 		assertThat(span.getTags()).containsEntry("username", "test");
 		assertThat(span.getTags()).doesNotContainKey("username_disclosed");
-		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IP));
+		assertThat(span.getTags()).containsEntry(Tags.PEER_HOST_IPV4.getKey(), InetAddresses.inetAddressToInt(IPV4));
 	}
 
-	private SpanWrapper createSpan() {
-		return createSpan("test", IP.getHostAddress());
+	private SpanWrapper createSpanIpv4() {
+		return createSpan("test", IPV4.getHostAddress());
 	}
 
 	private SpanWrapper createSpan(String username, String ip) {
-		final SpanWrapper span = new SpanWrapper(mockTracer.buildSpan("").start(), "", 0, 0, Collections.singletonList(new AnonymizingSpanEventListener(tracingPlugin)), new ConcurrentHashMap<>());
+		final SpanWrapper span = new SpanWrapper(
+				mockTracer.buildSpan("").start(),
+				"", 0, 0,
+				Collections.singletonList(new AnonymizingSpanEventListener(tracingPlugin)), new ConcurrentHashMap<>());
 		span.setTag(SpanUtils.USERNAME, username);
 		SpanUtils.setClientIp(span, ip);
 		span.finish();
