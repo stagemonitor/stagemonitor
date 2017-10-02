be177db9d39eed000053cf6da5a45d9e42974d65
Felix Barnsteiner <felix.barnsteiner@isys.de>
2017-10-02 18:04:05 +0200

    Remove dependency to Spring

--- stagemonitor-web-servlet/src/main/java/org/stagemonitor/web/servlet/util/AntPathMatcher.java
+++ stagemonitor-web-servlet/src/main/java/org/stagemonitor/web/servlet/util/AntPathMatcher.java
@@ -15,14 +15,14 @@
  */
 package org.stagemonitor.web.servlet.util;
 
-import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
-
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -907,4 +907,122 @@ public class AntPathMatcher {
 		}
 	}
 
+	private static class StringUtils {
+
+		/**
+		 * Tokenize the given {@code String} into a {@code String} array via a {@link StringTokenizer}. <p>The given {@code
+		 * delimiters} string can consist of any number of delimiter characters. Each of those characters can be used to
+		 * separate tokens. A delimiter is always a single character; for multi-character delimiters, consider using {@code
+		 * #delimitedListToStringArray}.
+		 *
+		 * @param str               the {@code String} to tokenize
+		 * @param delimiters        the delimiter characters, assembled as a {@code String} (each of the characters is
+		 *                          individually considered as a delimiter)
+		 * @param trimTokens        trim the tokens via {@link String#trim()}
+		 * @param ignoreEmptyTokens omit empty tokens from the result array (only applies to tokens that are empty after
+		 *                          trimming; StringTokenizer will not consider subsequent delimiters as token in the first
+		 *                          place).
+		 * @return an array of the tokens ({@code null} if the input {@code String} was {@code null})
+		 * @see java.util.StringTokenizer
+		 * @see String#trim()
+		 */
+		private static String[] tokenizeToStringArray(
+				String str, String delimiters, boolean trimTokens, boolean ignoreEmptyTokens) {
+
+			if (str == null) {
+				return null;
+			}
+			StringTokenizer st = new StringTokenizer(str, delimiters);
+			List<String> tokens = new ArrayList<String>();
+			while (st.hasMoreTokens()) {
+				String token = st.nextToken();
+				if (trimTokens) {
+					token = token.trim();
+				}
+				if (!ignoreEmptyTokens || token.length() > 0) {
+					tokens.add(token);
+				}
+			}
+			return toStringArray(tokens);
+		}
+
+		/**
+		 * Copy the given {@code Collection} into a {@code String} array. <p>The {@code Collection} must contain {@code
+		 * String} elements only.
+		 *
+		 * @param collection the {@code Collection} to copy
+		 * @return the {@code String} array ({@code null} if the supplied {@code Collection} was {@code null})
+		 */
+		private static String[] toStringArray(Collection<String> collection) {
+			if (collection == null) {
+				return null;
+			}
+			return collection.toArray(new String[collection.size()]);
+		}
+
+		/**
+		 * Check whether the given {@code CharSequence} contains actual <em>text</em>.
+		 * <p>More specifically, this method returns {@code true} if the
+		 * {@code CharSequence} is not {@code null}, its length is greater than
+		 * 0, and it contains at least one non-whitespace character.
+		 * <p><pre class="code">
+		 * StringUtils.hasText(null) = false
+		 * StringUtils.hasText("") = false
+		 * StringUtils.hasText(" ") = false
+		 * StringUtils.hasText("12345") = true
+		 * StringUtils.hasText(" 12345 ") = true
+		 * </pre>
+		 * @param str the {@code CharSequence} to check (may be {@code null})
+		 * @return {@code true} if the {@code CharSequence} is not {@code null},
+		 * its length is greater than 0, and it does not contain whitespace only
+		 * @see Character#isWhitespace
+		 */
+		public static boolean hasText(CharSequence str) {
+			if (!hasLength(str)) {
+				return false;
+			}
+			int strLen = str.length();
+			for (int i = 0; i < strLen; i++) {
+				if (!Character.isWhitespace(str.charAt(i))) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		/**
+		 * Check that the given {@code CharSequence} is neither {@code null} nor
+		 * of length 0.
+		 * <p>Note: this method returns {@code true} for a {@code CharSequence}
+		 * that purely consists of whitespace.
+		 * <p><pre class="code">
+		 * StringUtils.hasLength(null) = false
+		 * StringUtils.hasLength("") = false
+		 * StringUtils.hasLength(" ") = true
+		 * StringUtils.hasLength("Hello") = true
+		 * </pre>
+		 * @param str the {@code CharSequence} to check (may be {@code null})
+		 * @return {@code true} if the {@code CharSequence} is not {@code null} and has length
+		 * @see #hasText(String)
+		 */
+		public static boolean hasLength(CharSequence str) {
+			return (str != null && str.length() > 0);
+		}
+
+	}
+
+	private static class Assert {
+		/**
+		 * Assert that an object is not {@code null}.
+		 * <pre class="code">Assert.notNull(clazz, "The class must not be null");</pre>
+		 * @param object the object to check
+		 * @param message the exception message to use if the assertion fails
+		 * @throws IllegalArgumentException if the object is {@code null}
+		 */
+		public static void notNull(Object object, String message) {
+			if (object == null) {
+				throw new IllegalArgumentException(message);
+			}
+		}
+	}
 }
