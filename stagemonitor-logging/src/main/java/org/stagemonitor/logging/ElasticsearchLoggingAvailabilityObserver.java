1accdfb22c43e6fa39cae5309df9ee197cdd2742
Felix Barnsteiner <felix.barnsteiner@isys.de>
2018-01-10 09:03:44 +0100

    Remove misleading logger

--- stagemonitor-logging/src/main/java/org/stagemonitor/logging/ElasticsearchLoggingAvailabilityObserver.java
+++ stagemonitor-logging/src/main/java/org/stagemonitor/logging/ElasticsearchLoggingAvailabilityObserver.java
@@ -1,18 +1,13 @@
 package org.stagemonitor.logging;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.stagemonitor.core.elasticsearch.AbstractElasticsearchFirstAvailabilityObserver;
 import org.stagemonitor.core.elasticsearch.ElasticsearchClient;
 
 public class ElasticsearchLoggingAvailabilityObserver extends AbstractElasticsearchFirstAvailabilityObserver {
-	private final Logger logger = LoggerFactory.getLogger(ElasticsearchLoggingAvailabilityObserver.class);
 
 	@Override
 	protected void onElasticsearchFirstAvailable(ElasticsearchClient elasticsearchClient) {
-		logger.debug("sending grafana ElasticsearchLogging...");
 		elasticsearchClient.sendMetricDashboardBulkAsync("kibana/Logging.bulk");
-		logger.debug("sent grafana ElasticsearchLogging...");
 	}
 
 }
