4accbababc98869064173e494d2ada80a420a264
Igor Dianov <igor@dianov.com>
2019-02-09 15:23:40 -0800

    fix: add dummy test to bypass gradle no tests error

--- stagemonitor-benchmark/src/test/java/org/stagemonitor/InstrumentationPerformanceTest.java
+++ stagemonitor-benchmark/src/test/java/org/stagemonitor/InstrumentationPerformanceTest.java
@@ -1,22 +1,6 @@
 package org.stagemonitor;
 
-import com.codahale.metrics.Counter;
-import com.codahale.metrics.Gauge;
-import com.codahale.metrics.Histogram;
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.Timer;
-
-import org.apache.commons.io.FileUtils;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.node.InternalSettingsPreparer;
-import org.elasticsearch.plugins.Plugin;
-import org.elasticsearch.transport.Netty4Plugin;
-import org.slf4j.LoggerFactory;
-import org.stagemonitor.core.Stagemonitor;
-import org.stagemonitor.core.instrument.TimedElementMatcherDecorator;
-import org.stagemonitor.core.metrics.SortedTableLogReporter;
-import org.stagemonitor.core.metrics.metrics2.MetricName;
+import static org.stagemonitor.core.metrics.metrics2.MetricName.name;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,13 +10,29 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-import static org.stagemonitor.core.metrics.metrics2.MetricName.name;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Timer;
+import org.apache.commons.io.FileUtils;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.node.InternalSettingsPreparer;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.transport.Netty4Plugin;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+import org.stagemonitor.core.Stagemonitor;
+import org.stagemonitor.core.instrument.TimedElementMatcherDecorator;
+import org.stagemonitor.core.metrics.SortedTableLogReporter;
+import org.stagemonitor.core.metrics.metrics2.MetricName;
 
 public class InstrumentationPerformanceTest  {
 
 	private static Node node;
 
-	private InstrumentationPerformanceTest() {
+	public InstrumentationPerformanceTest() {
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -85,5 +85,9 @@ public class InstrumentationPerformanceTest  {
 		TimedElementMatcherDecorator.logMetrics();
 		Stagemonitor.reset();
 	}
-
+	
+	@Test
+	public void testDummy() {
+	    
+	}
 }
