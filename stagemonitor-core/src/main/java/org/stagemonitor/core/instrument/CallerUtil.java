5f228dd762f48efa8021ddbc283be77d7eb88239
Andreas Liebhäuser <andreas.liebhaeuser@isys-software.de>
2019-08-02 09:40:20 +0200

    #452 remove the sun.misc.Unsafe usage (#460)

--- stagemonitor-core/src/main/java/org/stagemonitor/core/instrument/CallerUtil.java
+++ stagemonitor-core/src/main/java/org/stagemonitor/core/instrument/CallerUtil.java
@@ -3,21 +3,8 @@ package org.stagemonitor.core.instrument;
 import org.stagemonitor.core.CorePlugin;
 import org.stagemonitor.core.Stagemonitor;
 import org.stagemonitor.core.metrics.aspects.SignatureUtils;
-import org.stagemonitor.core.util.ClassUtils;
-import sun.misc.JavaLangAccess;
-import sun.misc.SharedSecrets;
 
 public final class CallerUtil {
-
-	private static final Object javaLangAccessObject;
-
-	static {
-		if (ClassUtils.hasMethod("sun.misc.JavaLangAccess", "getStackTraceDepth", Throwable.class)) {
-			javaLangAccessObject = SharedSecrets.getJavaLangAccess();
-		} else {
-			javaLangAccessObject = null;
-		}
-	}
 
 	private CallerUtil() {
 	}
@@ -29,28 +16,7 @@ public final class CallerUtil {
 		if (Stagemonitor.getPlugin(CorePlugin.class).getIncludePackages().isEmpty()) {
 			return null;
 		}
-		if (javaLangAccessObject != null) {
-			return getCallerSignatureSharedSecrets();
-		} else {
-			return getCallerSignatureGetStackTrace();
-		}
-	}
 
-	private static String getCallerSignatureSharedSecrets() {
-		String executedBy = null;
-		Exception exception = new Exception();
-		final JavaLangAccess javaLangAccess = (JavaLangAccess) javaLangAccessObject;
-		for (int i = 2; i < javaLangAccess.getStackTraceDepth(exception); i++) {
-			final StackTraceElement stackTraceElement = javaLangAccess.getStackTraceElement(exception, i);
-			if (StagemonitorClassNameMatcher.isIncluded(stackTraceElement.getClassName())) {
-				executedBy = SignatureUtils.getSignature(stackTraceElement.getClassName(), stackTraceElement.getMethodName());
-				break;
-			}
-		}
-		return executedBy;
-	}
-
-	private static String getCallerSignatureGetStackTrace() {
 		String executedBy = null;
 		for (StackTraceElement stackTraceElement : Thread.currentThread().getStackTrace()) {
 			if (StagemonitorClassNameMatcher.isIncluded(stackTraceElement.getClassName())) {
@@ -60,4 +26,5 @@ public final class CallerUtil {
 		}
 		return executedBy;
 	}
+
 }
