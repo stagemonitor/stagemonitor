3cd361efe17809b4d15edf2536adada4699dda75
玉龙高 <gaoyulongchina@gmail.com>
2019-08-18 22:29:57 +0800

    revert

--- stagemonitor-alerting/src/main/java/org/stagemonitor/alerting/check/Threshold.java
+++ stagemonitor-alerting/src/main/java/org/stagemonitor/alerting/check/Threshold.java
@@ -105,9 +105,9 @@ public class Threshold {
 		/**
 		 * Checks, whether {@code actualValue OPERATOR thresholdValue} is true or false
 		 *
-		 * @param actualValue x
-		 * @param expectedValue x
-		 * @return x
+		 * @param actualValue
+		 * @param expectedValue
+		 * @return
 		 */
 		public abstract boolean check(double actualValue, double expectedValue);
 
